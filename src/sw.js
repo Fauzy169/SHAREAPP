7ab6abaefe068e547aaed45ae3f32633ad2cca93
Fauzy169 <ahmadfauzy169@gmail.com>
2025-05-28 21:11:37 +0800

    commit icon

--- src/sw.js
+++ src/sw.js
@@ -15,9 +15,13 @@ const ASSETS_TO_CACHE = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .catch(err => {
+        console.error('Cache error:', err);
+      })
   );
 });
+
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
